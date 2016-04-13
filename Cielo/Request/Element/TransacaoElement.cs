0f622f644ed9fbe15bb566069d949fbffc4ab2ad
ricardo ruas <ricardoruas88@gmail.com>
2016-04-13 11:23:10 -0300

    Melhorias do item 3.2.3 Renova Fácil com Token

--- Cielo/Request/Element/TransacaoElement.cs
+++ Cielo/Request/Element/TransacaoElement.cs
@@ -4,54 +4,63 @@ using System.Xml.Serialization;
 
 namespace Cielo.Request.Element
 {
-	[SerializableAttribute ()]
-	[XmlRootAttribute ("transacao", Namespace = "http://ecommerce.cbmp.com.br", IsNullable = false)]
-	public partial class TransacaoElement :AbstractElement
-	{
-		public String tid { get; set; }
+    [SerializableAttribute()]
+    [XmlRootAttribute("transacao", Namespace = "http://ecommerce.cbmp.com.br", IsNullable = false)]
+    public partial class TransacaoElement : AbstractElement
+    {
+        public String tid { get; set; }
 
-		public String pan { get; set; }
+        public String pan { get; set; }
 
-		[XmlElementAttribute ("dados-pedido")]
-		public DadosPedidoElement dadosPedido { get; set; }
+        [XmlElementAttribute("dados-pedido")]
+        public DadosPedidoElement dadosPedido { get; set; }
 
-		[XmlElementAttribute ("forma-pagamento")]
-		public FormaPagamentoElement formaPagamento { get; set; }
+        [XmlElementAttribute("forma-pagamento")]
+        public FormaPagamentoElement formaPagamento { get; set; }
 
-		public String status;
+        public String status;
 
-		public AutenticacaoElement autenticacao { get; set; }
+        public AutenticacaoElement autenticacao { get; set; }
 
-		public AutorizacaoElement autorizacao { get; set; }
+        public AutorizacaoElement autorizacao { get; set; }
 
-		[XmlElementAttribute ("url-autenticacao")]
-		public String urlAutenticacao;
+        [XmlElementAttribute("url-autenticacao")]
+        public String urlAutenticacao;
+
+        public TokenElement token { get; set; }
 
 
-		public static Transaction unserialize (Transaction transaction, String response)
-		{
-			TransacaoElement transacao = new TransacaoElement ();
-			transacao = transacao.unserializeElement (transacao, response);
+        public static Transaction unserialize(Transaction transaction, String response)
+        {
+            TransacaoElement transacao = new TransacaoElement();
+            transacao = transacao.unserializeElement(transacao, response);
 
-			Console.Write (response);
+            Console.Write(response);
 
             if (transaction == null)
                 transaction = new Transaction();
 
-			transaction.tid = transacao.tid;
-			transaction.pan = transacao.pan;
-			transaction.order = transacao.dadosPedido.ToOrder ();
-			transaction.paymentMethod = transacao.formaPagamento.ToPaymentMethod ();
+            transaction.tid = transacao.tid;
+            transaction.pan = transacao.pan;
+            transaction.order = transacao.dadosPedido.ToOrder();
+            transaction.paymentMethod = transacao.formaPagamento.ToPaymentMethod();
 
-			if (transacao.autenticacao != null) {
-				transaction.authentication = transacao.autenticacao.ToAuthentication ();
-			}
+            if (transacao.autenticacao != null)
+            {
+                transaction.authentication = transacao.autenticacao.ToAuthentication();
+            }
 
-			if (transacao.autorizacao != null) {
-				transaction.authorization = transacao.autorizacao.ToAuthorization ();
-			}
+            if (transacao.autorizacao != null)
+            {
+                transaction.authorization = transacao.autorizacao.ToAuthorization();
+            }
 
-			return transaction;
-		}
+            if (transacao.token != null)
+            {
+                transaction.token = transacao.token.ToToken();
+            }
+
+            return transaction;
+        }
     }
 }
