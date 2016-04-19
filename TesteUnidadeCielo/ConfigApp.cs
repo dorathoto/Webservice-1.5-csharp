5536867884dc656d349fdeee34f35af978951e14
dorathoto <leonardo.dorathoto@gmail.com>
2016-04-19 10:54:18 -0300

    Criando UnitTest

---
+++ TesteUnidadeCielo/ConfigApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cielo
+{
+    public class ConfigApp
+    {
+
+        //DEBUG
+        internal const string key = "25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3";
+        internal const string mid = "1006993069"; //id loja
+        internal const string UrlCieloEcommerce = Cielo.TEST;
+        internal const string UrlRetornoAPP = "https://localhost:47451/";
+
+
+        //Produção
+    }
+}
---
+++ TesteUnidadeCielo/Teste.cs
@@ -0,0 +1,77 @@
+﻿using Cielo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TesteUnidadeCielo
+{
+    [TestClass]
+    public class Teste
+    {
+        [TestMethod]
+        public void TestarTransacao()
+        {
+            var cielo = new Cielo.Cielo(ConfigApp.mid, ConfigApp.key, ConfigApp.UrlCieloEcommerce);
+
+            var holder = cielo.holder("4012001038443335", "2018", "05", "123");
+            holder.name = "Fulano Portador da Silva";
+
+            var randomOrder = new Random();
+
+            var order = cielo.order(randomOrder.Next(1000, 10000).ToString(), 10000);
+            var paymentMethod = cielo.paymentMethod(PaymentMethod.VISA, PaymentMethod.CREDITO_A_VISTA);
+
+            var transaction = cielo.transactionRequest(
+                holder,
+                order,
+                paymentMethod,
+                "http://localhost/cielo",
+                Transaction.AuthorizationMethod.AUTHORIZE_WITHOUT_AUTHENTICATION,
+                false
+            );
+
+            var codigoLr = transaction.authorization.lr;
+
+            Assert.AreEqual(codigoLr, 0);
+        }
+
+        [TestMethod]
+        public void TestarTransacaoComToken()
+        {
+            var cielo = new Cielo.Cielo(ConfigApp.mid, ConfigApp.key, ConfigApp.UrlCieloEcommerce);
+
+            var holder = cielo.holder("4012001038443335", "2018", "05", "123");
+            holder.name = "Fulano Portador da Silva";
+
+            var randomOrder = new Random();
+
+            var order = cielo.order(randomOrder.Next(1000, 10000).ToString(), 10000);
+            var paymentMethod = cielo.paymentMethod(PaymentMethod.VISA, PaymentMethod.CREDITO_A_VISTA);
+
+            var transaction = cielo.transactionRequest(
+                holder,
+                order,
+                paymentMethod,
+                "http://localhost/cielo",
+                Transaction.AuthorizationMethod.AUTHORIZE_WITHOUT_AUTHENTICATION,
+                false
+            );
+
+            var Token = transaction.token;
+
+            Assert.AreEqual(Token.code, "!#$!#SFDA@#$@");
+        }
+
+        [TestMethod]
+        public void TestarCancelarTransacao()
+        {
+            var cielo = new Cielo.Cielo(ConfigApp.mid, ConfigApp.key, ConfigApp.UrlCieloEcommerce);
+
+            var tid = "1100000000$A5E";
+            var valorCancelar = 0; //0 igual cancelamento total
+
+            var transacao = cielo.cancellationRequest(tid, valorCancelar);
+
+            Assert.AreEqual(transacao, null);
+        }
+    }
+}
